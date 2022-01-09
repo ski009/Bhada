7c11f5941ff6f3eb950bdf820cc18ad3921aebbb
ski009 <s.mridulshukla@gmail.com>
2022-01-09 23:57:25 +0530

    heroku updates

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/bhada/index.html'));});
-app.listen(process.env.PORT || 8080);
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', (req,res,next) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+
+app.listen(process.env.PORT || 8000);
