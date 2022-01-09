9b9955e3077756427efba035d1697086c78e21b7
ski009 <s.mridulshukla@gmail.com>
2022-01-09 23:10:25 +0530

    refactoring

--- src/app/config/config.serivice.ts
+++ src/app/config/config.serivice.ts
@@ -9,25 +9,28 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
   private configUrl = ' ';
-  const httpOptions = {
+
+  constructor(private httpClient: HttpClient) { }
+
+  private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type':  'application/json',
       Authorization: 'my-auth-token'
     })
   };
 
-  constructor(private httpClient: HttpClient) { }
+
 
   getConfig() {
    return this.httpClient.get<Config>(this.configUrl);
   }
 
   loginApi(user: User): Observable<Hero> {
-    
-    httpOptions.headers =
-  httpOptions.headers.set('Authorization', 'my-new-auth-token');
 
-    return this.http.post<Hero>(this.configUrl, user, httpOptions)
+    httpOptions.headers =
+    httpOptions.headers.set('Authorization', 'my-new-auth-token');
+
+    return this.http.post<User>(this.configUrl, user, httpOptions)
       .pipe(
         catchError(this.handleError('user', user))
       );
